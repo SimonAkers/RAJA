b9a925df3c3d6b49b3de47e87b9a39fd1e535ae8
Shayes <hayesakers@gmail.com>
2022-09-12 13:07:47 -0400

    Replaced main entry point with prototype GUI.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,99 @@
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    // if let Err(e) = run() {
-    //     eprintln!("ERROR: {}", e);
-    // }
+use gtk::prelude::*;
+use sourceview5::prelude::*;
 
-    use eframe::egui::Visuals;
-    let app = simulator::App::default();
-    let opts = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Just Another Mips Editor and Simulator",
-        opts,
-        Box::new(|cc| {
-            cc.egui_ctx.set_visuals(Visuals::dark());
-            Box::new(app)
-        }),
+use adw::{Application, ApplicationWindow, HeaderBar, ColorScheme, StyleManager};
+use adw::gio::MenuModel;
+use gtk::{Builder, Button, Orientation, Paned, Picture, PopoverMenuBar, Scrollable, Separator, TextView, Widget};
+use gtk::{CssProvider, ScrollablePolicy, ScrolledWindow, StyleContext};
+use gtk::builders::BoxBuilder;
+use gtk::gdk::Display;
+use sourceview5::StyleSchemeChooserButton;
+
+fn main() {
+    let app = Application::builder()
+        .application_id("net.shayes.raja")
+        .flags(Default::default())
+        .build();
+
+    sourceview5::View::ensure_type();
+    sourceview5::Buffer::ensure_type();
+    sourceview5::Language::ensure_type();
+
+    app.connect_startup(|_| load_css());
+    app.connect_activate(build_from_xml);
+
+    app.run();
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = CssProvider::new();
+    provider.load_from_data(include_bytes!("../res/style.css"));
+
+    // Add the provider to the default screen
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
     );
+}
+
+fn build_from_xml(app: &Application) {
+    StyleManager::default().set_color_scheme(ColorScheme::PreferDark);
+
+    // Build UI from the specification
+    let builder = Builder::from_file("res/ui/main.ui");
+
+    // Style the source view
+    let srcview: sourceview5::View = builder.object("source_view").unwrap();
+    style_srcview(&srcview);
+
+    // Style the console
+    let console: sourceview5::View = builder.object("console").unwrap();
+    style_console(&console);
+
+    // Get the main widget from the builder
+    let content: Widget = builder.object("main_box").unwrap();
+
+    // Create the window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .default_width(960)
+        .default_height(540)
+        .width_request(400)
+        .height_request(300)
+        .title("RAJA")
+        .content(&content)
+        .build();
+
+    // Show the window
+    window.show();
+}
+
+/// Styles a GtkSourceView as the MIPS code view
+/// Probably can be consolidated
+fn style_srcview(srcview: &sourceview5::View) {
+    let buffer = sourceview5::Buffer::new(None);
+
+    if let Some(ref language) = sourceview5::LanguageManager::new().language("mal") {
+        buffer.set_language(Some(language));
+    }
+
+    if let Some(ref scheme) = sourceview5::StyleSchemeManager::new().scheme("Adwaita-dark") {
+        buffer.set_style_scheme(Some(scheme));
+    }
+
+    srcview.set_buffer(Some(&buffer));
+}
+
+/// Styles a GtkSourceView as the console
+/// Probably can be consolidated
+fn style_console(console: &sourceview5::View) {
+    let buffer = sourceview5::Buffer::new(None);
+
+    if let Some(ref scheme) = sourceview5::StyleSchemeManager::new().scheme("Adwaita-dark") {
+        buffer.set_style_scheme(Some(scheme));
+    }
+
+    console.set_buffer(Some(&buffer));
 }
