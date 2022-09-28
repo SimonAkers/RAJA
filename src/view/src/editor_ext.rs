62f3c05b3a6badc4b8e6f1f93cf2293de76a8dd5
Shayes <hayesakers@gmail.com>
2022-09-28 19:38:28 -0400

    Fixed recursive call in EditorExt and added some documentation.

--- src/view/src/editor_ext.rs
+++ src/view/src/editor_ext.rs
@@ -1,14 +1,25 @@
+use gtk::prelude::*;
+
 pub trait EditorExt {
     fn get_text(&self) -> String;
     fn clear(&self);
 }
 
 impl EditorExt for sourceview5::View {
+    /// Gets the text of the GtkSourceView
+    ///
+    /// # Returns
+    /// The text of the GtkSourceView as a String
     fn get_text(&self) -> String {
-        self.get_text()
+        // Get the bounds of the buffer
+        let (iter1, iter2) = self.buffer().bounds();
+
+        // Return the text within the buffer bounds
+        self.buffer().text(&iter1, &iter2, false).as_str().to_owned()
     }
 
+    /// Clears the buffer of the GtkSourceView (sets it to the empty string)
     fn clear(&self) {
-        self.clear();
+        self.buffer().set_text("");
     }
 }
