e72aaccf7f8f914516307bf424a7d19a20045edf
Shayes <hayesakers@gmail.com>
2022-12-04 17:33:43 -0500

    Added documentation for `gtk_console` module.

--- src/view/src/gtk_console/mod.rs
+++ src/view/src/gtk_console/mod.rs
@@ -7,28 +7,41 @@ use gtk::TextBuffer;
 use crate::traits::Console;
 
 glib::wrapper! {
+    /**
+    A custom widget which represents a graphical console for text I/O.
+
+    # See also:
+    - [GtkConsoleTemplate][`crate::gtk_console::template::GtkConsoleTemplate`]
+    - [Console][`crate::traits::Console`]
+     */
     pub struct GtkConsole(ObjectSubclass<template::GtkConsoleTemplate>)
         @extends gtk::TextView, gtk::Widget,
         @implements gtk::Accessible, gtk::Actionable, gtk::Buildable, gtk::ConstraintTarget;
 }
 
+/// See [Console][`crate::traits::Console`] for docs.
 impl Console for GtkConsole {
     fn print(&self, msg: &str) {
         // Insert the message at the end of the buffer
         self.buffer().insert(&mut self.buffer().end_iter(), msg);
+        // Scroll the widget to the bottom
         self.scroll_to_iter(&mut self.buffer().end_iter(), 0.0, true, 0.0, 0.0);
     }
 
     fn print_err(&self, msg: &str) {
         let buffer = self.buffer();
 
+        // Print the message
         self.print(msg);
 
+        // Get the bounds of the message we just printed
+        // (this must be done after modifying the buffer)
         let start_iter = buffer.iter_at_offset(buffer.char_count() - msg.len() as i32);
         let end_iter = buffer.end_iter();
 
+        // Create a tag with a foreground color
         let tag = buffer.create_tag(None, &[("foreground", &"#FF3535")]);
-
+        // Apply the tag to color the message
         buffer.apply_tag(&tag.unwrap(), &start_iter, &end_iter);
     }
 
@@ -37,6 +50,7 @@ impl Console for GtkConsole {
     }
 
     fn clear(&self) {
+        // Set the buffer to the default TextBuffer (i.e. reset it)
         self.set_buffer(Some(&TextBuffer::default()));
     }
 }
--- src/view/src/gtk_console/template.rs
+++ src/view/src/gtk_console/template.rs
@@ -1,9 +1,16 @@
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 
+/**
+The template for [GtkConsole][`crate::gtk_console::GtkConsole`] \
+which represents a console widget.
+
+This mostly consists of gtk-rs boilerplate and should not be constructed directly.
+ */
 #[derive(Default)]
 pub struct GtkConsoleTemplate;
 
+/// gtk-rs boilerplate implementation
 #[glib::object_subclass]
 impl ObjectSubclass for GtkConsoleTemplate {
     // `NAME` needs to match `class` attribute of template
