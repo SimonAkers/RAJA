2a0b87208ca561a508bb9ba5673c86d0db23af0b
Shayes <hayesakers@gmail.com>
2022-12-04 17:36:12 -0500

    Added documentation for ensure.rs

--- src/view/src/ensure.rs
+++ src/view/src/ensure.rs
@@ -2,6 +2,10 @@ use glib::*;
 
 use crate::gtk_console::GtkConsole;
 
+/**
+Ensures that custom widgets are known to GTK. Must be called before an instance
+of [`gtk::Application`] is created.
+ */
 pub fn ensure_types() {
     GtkConsole::ensure_type();
 }
