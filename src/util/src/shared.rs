7a85e6546a7d5c51767a3abc604793d5d0395c03
Shayes <hayesakers@gmail.com>
2022-12-04 20:27:20 -0500

    Added documentation to shared.rs

--- src/util/src/shared.rs
+++ src/util/src/shared.rs
@@ -1,29 +1,37 @@
-/*!
-A simple wrapper for saying Rc::new(RefCell::new(Object)).
-
-Based on https://gist.github.com/stevedonovan/7e3a6d8c8921e3eff16c4b11ab82b8d7
-*/
-
 use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 
+/**
+A simple wrapper for saying Rc::new(RefCell::new(Object)).
+
+Based on https://gist.github.com/stevedonovan/7e3a6d8c8921e3eff16c4b11ab82b8d7
+ */
 pub struct Shared<T> {
     rc: Rc<RefCell<T>>
 }
 
 impl<T> Shared<T> {
-    pub fn new(data: T) -> Self {
-        Self { rc: Rc::new(RefCell::new(data)) }
+    /**
+    Returns a new Shared instance containing a given object.
+
+    # Arguments
+    - `object` - The object to wrap.
+     */
+    pub fn new(object: T) -> Self {
+        Self { rc: Rc::new(RefCell::new(object)) }
     }
 
+    /// Returns a borrowed reference to the shared object.
     pub fn borrow(&self) -> Ref<T> {
         self.rc.borrow()
     }
 
+    /// Returns a borrowed mutable reference to the shared object.
     pub fn borrow_mut(&self) -> RefMut<T> {
         self.rc.borrow_mut()
     }
 
+    /// Returns a mutable pointer to the shared object.
     pub fn as_ptr(&self) -> *mut T {
         self.rc.as_ptr()
     }
